0dd3385075815015e549e3f9a335aa2e23113fa4
Mario Moreno <mario_morenor@hotmail.com>
2022-12-01 11:39:52 -0500

    Agreagado apartado de Edición

--- src/views/EditUserPage.vue
+++ src/views/EditUserPage.vue
@@ -4,29 +4,51 @@
       <ion-toolbar>
         <ion-buttons>
           <ion-back-button></ion-back-button>
+          <ion-title>Actualizar usuario</ion-title>
         </ion-buttons>
       </ion-toolbar>
     </ion-header>
     <ion-content>
-      <ion-item>
-        <ion-label>
-          <h2>Usuario: {{ user.username }}</h2>
-          <p>Correo: {{ user.email }}</p>
-        </ion-label>
-      </ion-item>
-      <ion-item>
-        <ion-label position="stacked">Contraseña:</ion-label>
-        <ion-input v-model="user.value"></ion-input>
-      </ion-item>
-      <ion-button @click="updatePassword()" color="primary" expand="block"
-        >ACTUALIZAR</ion-button
-      >
+      <ion-loading :is-open="searching" message="Guardando Usuario, Espere por favor no sea apurado...">
+      </ion-loading>
+      <form id="form">
+        <ion-item>
+          <ion-label>Usuario:</ion-label>
+          <ion-input @change="setEmail()" required v-model="user.username"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label>Cédula:</ion-label>
+          <ion-input @change="setPossiblyPassword()" required type="number" v-model="user.identity"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label>Contraseña:</ion-label>
+          <ion-input required v-model="user.password"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label>Email:</ion-label>
+          <ion-input type="email" v-model="user.email"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label>Nombres:</ion-label>
+          <ion-input required v-model="user.name"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label>Apellidos:</ion-label>
+          <ion-input required v-model="user.last_name"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label>Carrera:</ion-label>
+          <ion-input v-model="user.career"></ion-input>
+        </ion-item>
+        <ion-button type="submit" expand="block" @click="saveUser()">ACTUALIZAR <ion-icon class="ion-padding-start"
+            :icon="save"></ion-icon>
+        </ion-button>
+      </form>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { Http, HttpResponse } from "@capacitor-community/http";
 import {
   IonPage,
   IonHeader,
@@ -38,28 +60,38 @@ import {
   IonButton,
   toastController,
   IonBackButton,
-  IonButtons
+  IonButtons,
+  IonIcon,
+  IonLoading,
+  IonTitle
 } from "@ionic/vue";
 import { defineComponent } from "vue";
 
-import { store,st } from "../store";
+import { save } from "ionicons/icons";
 
-type User = {
-  id: number;
-  username: string;
-  value: string;
-  name: string;
-  last_name: string;
-  status: number;
-};
+import { store, st } from "../store";
+import { Http, HttpResponse } from "@capacitor-community/http";
 
 export default defineComponent({
-  name: "EditPage",
-
+  name: "EditUserPage",
+  setup() {
+    return {
+      save,
+    };
+  },
   data() {
     return {
       store,
-      user: {} as User,
+      user: {
+        username: "",
+        password: "",
+        identity: "",
+        email: "",
+        name: "",
+        last_name: "",
+        career: ""
+      },
+      searching: false
     };
   },
   components: {
@@ -72,41 +104,84 @@ export default defineComponent({
     IonItem,
     IonButton,
     IonBackButton,
-    IonButtons
+    IonButtons,
+    IonIcon,
+    IonLoading,
+    IonTitle
   },
   mounted() {
-    this.user = this.store.user;
+    let form = document.getElementById("form");
+
+    form?.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+
+    this.user = Object.assign({ password: this.store.user.value, career: this.store.user['career-unit'] }, { ...this.store.user })
   },
   methods: {
     home() {
       this.$router.back();
     },
-    async updatePassword() {
-      const options = {
-        url: `${this.store.host}/updatePassword`,
+    async saveUser() {
+      if (
+        !this.user.username ||
+        !this.user.password ||
+        !this.user.identity ||
+        !this.user.email ||
+        !this.user.name ||
+        !this.user.last_name
+      ) {
+        return;
+      }
+
+      this.searching = true;
+
+      let options = {
+        url: `${this.store.host}/updateUser`,
         headers: {
           "Content-Type": "application/json",
         },
-        data: {
-          user: this.user,
-        },
+        data: { user: this.user },
       };
       const toast = await toastController.create({
-        message: "Se ha cambiado la contraseña con éxito",
-        duration: 1500,
-        position: "middle",
+        duration: 2000,
+        position: "bottom",
       });
-      Http.post(options).then((res) => {
-        this.store.users.forEach((u) => {
-          if (u.id == this.user.id) {
-            u.value = this.user.value;
-          }
-        });
+      const result: HttpResponse = await Http.post(options);
+      if (result.status == 500) {
+        toast.color = "danger";
+        toast.message =
+          "Error Al insertar el usuario, verifique que el usuario no exista en la base de datos";
         toast.present();
-      });
+        this.searching = false
+        return;
+      }
+      toast.message = `Se ha actualizado al usuario ${this.user.username} con éxito`;
+      toast.color = "success";
+      toast.present();
+      this.resetUser();
+      this.searching = false
     },
+    resetUser() {
+      this.store.user.value = this.user.password;
+      this.store.user.name = this.user.name;
+      this.store.user.last_name = this.user.last_name;
+      this.store.user.username = this.user.username;
+      this.store.user.identity = this.user.identity;
+      this.store.user["career-unit"] = this.user.career;
+      console.log(this.store.user);
+      
+    },
+    setPossiblyPassword() {
+      this.user.password = this.user.identity
+    },
+    setEmail() {
+      this.user.email = `${this.user.username}@pucesd.edu.ec`
+    }
   },
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
